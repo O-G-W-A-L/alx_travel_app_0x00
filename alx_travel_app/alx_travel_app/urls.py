e0f01e388fb7649d8db8ac3a2149284c13cae7ae
O-G-W-A-L <ogwal04@gmail.com>
2025-05-30 18:32:57 +0300

    Database Modeling and Data Seeding in Django

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -6,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="ALX Travel 0x00 API",
+      title="ALX Travel API",
       default_version='v1',
       description="Endpoints for travel listings",
    ),
@@ -15,7 +14,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
